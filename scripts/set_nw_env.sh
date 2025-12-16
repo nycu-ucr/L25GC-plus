45006087c868ab53a046974bc0a94a870a35004c
p_dutta <p_dutta@node2.p-dutta-284623.uky-cloudnetsys-pg0.wisc.cloudlab.us>
2025-12-15 23:19:39 -0600

    temp files commit

--- scripts/set_nw_env.sh
+++ scripts/set_nw_env.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/bash 
+
 # UE/AN
-export UEIF='ens1f0'
-export UEIF_IP='192.168.1.1'
-export UEIF_IP_ORIGIN='192.168.1.1'
-export UEIF_SUBNET='192.168.1.0'
+export UEIF="enp94s0f1"
+export UEIF_IP_ORIGIN="192.168.1.1"
+export UEIF_IP="192.168.1.1"
+export UEIF_SUBNET="192.168.1.0"
+
 # DN
-export DNIF='ens1f1'
-export DNIF_IP='10.10.1.2'
+export DNIF='enp94s0f0'
+export DNIF_IP='192.168.1.4'
 export DNIF_IP_ORIGIN='192.168.1.4'
-export DNIF_SUBNET='10.10.1.0'
+export DNIF_SUBNET='192.168.1.0'
+
 # CN
-export CNANIF='ens1f0'
+export CNANIF='enp94s0f1'
 export CNANIF_IP='192.168.1.2'
-export CNANIF_MAC='3c:fd:fe:b4:ff:40'
+export CNANIF_MAC='3c:fd:fe:b3:17:4d'
 export CNANIF_ORIGIN_IP='192.168.1.2'
-export CNDNIF='ens1f1'
-export CNDNIF_IP='10.10.1.1'
-export CNDNIF_MAC='3c:fd:fe:b4:ff:41'
+export CNDNIF='enp94s0f0'
+export CNDNIF_IP='192.168.1.3'
+export CNDNIF_MAC='3c:fd:fe:b3:17:4c'
 export CNDNIF_ORIGIN_IP='192.168.1.3'
+
 # 5G constants
 export UPF_IP='192.168.1.2'
 export UE_IP='10.60.0.1'
